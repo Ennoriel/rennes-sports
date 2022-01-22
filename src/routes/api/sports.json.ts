0d3cf70f3f928646eb122900645a956ad2abeab2
Maxime Dupont <maxime.dupont@jamespot.com>
2022-01-22 21:34:49 +0100

    Minor fix

--- src/routes/api/sports.json.ts
+++ src/routes/api/sports.json.ts
@@ -3,19 +3,19 @@ import mongoUtil from '$lib/utils/db';
 
 export const get: RequestHandler = async () => {
 	return {
-		body: await mongoUtil.db.collection('sports').find({}).toArray()
+		body: await mongoUtil.db?.collection('sports').find({})?.toArray()
 	};
 };
 
 export const post: RequestHandler = async ({ request }) => {
 	const body = await request.json();
 	return {
-		body: await mongoUtil.db.collection('sports').insertOne(body)
+		body: await mongoUtil.db?.collection('sports')?.insertOne(body) as Record<string, any>
 	};
 };
 
 export const del: RequestHandler = async () => {
 	return {
-		body: await mongoUtil.db.collection('sports').deleteMany()
+		body: await mongoUtil.db?.collection('sports')?.deleteMany({}) as Record<string, any>
 	};
 };
