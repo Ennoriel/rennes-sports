d9598248ae7bb3ff95548e63861b0093a07e5570
Maxime Dupont <maxime.dupont@jamespot.com>
2022-03-15 21:24:12 +0100

    Fix bcrypt error

--- src/lib/utils/db.ts
+++ src/lib/utils/db.ts
@@ -1,24 +1,5 @@
-// import { Db, MongoClient, MongoClientOptions } from 'mongodb';
-//
-// class MongoUtil {
-// 	db: Db;
-// 	client: MongoClient;
-//
-// 	async init() {
-// 		if (!this.db) {
-// 			this.client = new MongoClient(
-// 				import.meta.env.VITE_MONGO_URI as string,
-// 				{ useNewUrlParser: true, useUnifiedTopology: true } as MongoClientOptions
-// 			);
-// 			await this.client.connect();
-// 			this.db = this.client.db(import.meta.env.VITE_MONGO_DB as string);
-// 		}
-// 	}
-// }
-//
-// export default new MongoUtil();
-
-import { MongoClient, MongoClientOptions } from 'mongodb';
+import { MongoClient } from 'mongodb';
+import type { MongoClientOptions } from 'mongodb';
 
 const uri = import.meta.env.VITE_MONGO_URI as string;
 const options = {
