3a36b3eb40e5c5f5730a47893a691336087f4bfa
Maxime Dupont <maxime.dupont@jamespot.com>
2022-05-10 22:30:42 +0200

    Fix jwt use

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,11 @@
 import { parse } from 'cookie';
-import { verify } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import type { GetSession, Handle } from '@sveltejs/kit';
 import type { LocalsSession } from '$lib/types/session.type';
 
 const verifySession = (token: string): LocalsSession => {
 	try {
-		return verify(token, import.meta.env.VITE_JWT_SECRET as string) as unknown as LocalsSession;
+		return jwt.verify(token, import.meta.env.VITE_JWT_SECRET as string) as unknown as LocalsSession;
 	} catch (ex) {
 		return null;
 	}
