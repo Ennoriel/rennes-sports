7a17290323ba0c9906a9013395d8eefdaecc7d85
Maxime Dupont <maxime.dupont@jamespot.com>
2022-06-04 20:28:33 +0200

    Fix sveltekit check hint about transition method not having 2 params but being called with 2

--- src/lib/component/svg/XOrMenu.svelte
+++ src/lib/component/svg/XOrMenu.svelte
@@ -3,10 +3,10 @@
 
 	export let visible = false;
 
-	function tr(node: SVGElement) {
+	function tr(node: SVGElement, { delay, duration }: { delay: number; duration: number }) {
 		return {
-			delay: 0,
-			duration: 200,
+			delay,
+			duration,
 			tick: (t: number) => {
 				(node.childNodes[0] as SVGElement).setAttribute('d', `M5 5 L12 ${12 - 7 * t} L19 5`);
 				(node.childNodes[1] as SVGElement).setAttribute('d', `M${12 - 7 * t} 12 L${12 + 7 * t} 12`);
@@ -29,7 +29,7 @@
 	class="feather feather-x"
 >
 	{#if visible}
-		<g transition:tr|local>
+		<g transition:tr|local={{ delay: 0, duration: 200 }}>
 			<path d="M5 5 L19 5" />
 			<path d="M5 12 L19 12" />
 			<path d="M5 19 L19 19" />
